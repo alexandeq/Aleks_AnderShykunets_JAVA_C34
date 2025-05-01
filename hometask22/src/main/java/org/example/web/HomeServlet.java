6031553deb9f63876aeba81d1e5f43a7da49a9a7
Aleks Ander <aleksandershykunets@gmail.com>
2025-05-01 22:40:48 +0300

    hometask22

---
+++ hometask22/src/main/java/org/example/web/HomeServlet.java
@@ -0,0 +1,20 @@
+package org.example.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("""
+               <body>Good luck<body>
+               """);
+    }
+}
