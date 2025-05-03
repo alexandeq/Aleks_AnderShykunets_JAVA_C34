86846e716ed7d4cbcae6f1dc4dadf9f8b1c45d53
Aleks Ander <aleksandershykunets@gmail.com>
2025-05-03 21:40:09 +0300

    hometask22

--- hometask22/src/main/java/org/example/web/HomeServlet.java
+++ hometask22/src/main/java/org/example/web/HomeServlet.java
@@ -7,14 +7,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+
         PrintWriter writer = resp.getWriter();
-        writer.println("""
-               <body>Good luck<body>
-               """);
+        writer.println("<body>Local date is: " + localDate + "<br/>Local time is: " + localTime + "</body>");
+
     }
 }
