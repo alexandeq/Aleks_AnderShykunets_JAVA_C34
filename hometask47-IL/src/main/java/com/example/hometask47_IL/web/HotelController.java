d05a5b4fc5ddff51ff0ac0f3aa5aa3cc76f4fdf2
Aleks Ander <aleksandershykunets@gmail.com>
2025-08-18 11:30:26 +0300

    hometask49

--- hometask47-IL/src/main/java/com/example/hometask47_IL/web/HotelController.java
+++ hometask47-IL/src/main/java/com/example/hometask47_IL/web/HotelController.java
@@ -1,7 +1,13 @@
 package com.example.hometask47_IL.web;
 
+import com.example.hometask47_IL.dto.ErrorDto;
 import com.example.hometask47_IL.dto.HotelDto;
 import com.example.hometask47_IL.service.HotelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +21,23 @@ public class HotelController {
     private final HotelService service;
 
     @PostMapping
+    @Operation(summary = "Create hotel", description = "Create hotel", tags = {"create"},
+            responses = {
+                    @ApiResponse(description = "hotel created", responseCode = "200", content = @Content(schema = @Schema(implementation = HotelDto.class))),
+                    @ApiResponse(description = "hotel already registered", responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+                    @ApiResponse(description = "unknown error", responseCode = "500", content = @Content())
+            })
     public HotelDto createHotel(@RequestBody HotelDto dto){
         return service.createHotel(dto);
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get all hotels", description = "List of all hotels", tags = {"read"},
+            responses = {
+                    @ApiResponse(description = "all hotels description", responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = HotelDto.class)))),
+                    @ApiResponse(description = "hotel list is disabled", responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+                    @ApiResponse(description = "unknown error", responseCode = "500", content = @Content())
+            })
     public List<HotelDto> getAllHotels() {
         return service.getAllHotels();
     }
